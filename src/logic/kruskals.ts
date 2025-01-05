d127ffa502bc9af230a9facf9d3878e64c33670e
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2025-01-04 23:36:28 -0500

    Improved performance

--- src/logic/kruskals.ts
+++ src/logic/kruskals.ts
@@ -7,11 +7,12 @@ export function createKruskalsMaze(width: number, height: number): Maze {
   // 1 Create a list of all walls, and create a set for each cell, each
   // containing just that one cell
   const edges = getEdges(maze);
-  const sets: Set<string>[] = [];
+  // Key: stringified Coordinate -> Value: Set of stringified Coordinates
+  const setMap: Map<string, Set<string>> = new Map<string, Set<string>>();
   for (let row = 0; row < maze.length; row++) {
     for (let col = 0; col < maze[0].length; col++) {
       const coord: Coordinate = { row, col };
-      sets.push(new Set([JSON.stringify(coord)]));
+      setMap.set(JSON.stringify(coord), new Set([JSON.stringify(coord)]));
     }
   }
 
@@ -20,23 +21,19 @@ export function createKruskalsMaze(width: number, height: number): Maze {
     const index = genRandomInt(0, edges.length);
     const edge = edges.splice(index, 1)[0];
 
-    const isConnected = sets.some(
-      (set) =>
-        set.has(JSON.stringify(edge.c1)) && set.has(JSON.stringify(edge.c2)),
-    );
+    const set1 = setMap.get(JSON.stringify(edge.c1))!;
+    const set2 = setMap.get(JSON.stringify(edge.c2))!;
+
+    const isConnected =
+      set1.has(JSON.stringify(edge.c2)) && set2.has(JSON.stringify(edge.c1));
 
     // 2.1 If the cells divided by this wall belong to distinct sets:
     if (!isConnected) {
       // 2.1.1 Remove the current wall
       maze = removeWallAtEdge(maze, edge);
       // 2.1.2 Join the sets of the formerly divided cells
-      const index1 = sets.findIndex((set) => set.has(JSON.stringify(edge.c1)));
-      const set1 = sets.splice(index1, 1)[0];
-      const index2 = sets.findIndex((set) => set.has(JSON.stringify(edge.c2)));
-      const set2 = sets.splice(index2, 1)[0];
-
       const newSet = new Set([...set1, ...set2]);
-      sets.push(newSet);
+      newSet.forEach((element) => setMap.set(element, newSet));
     }
   }
 
