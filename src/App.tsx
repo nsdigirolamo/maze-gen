2cbccddf5521ca01aec8ff3712afc27e718b7770
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2025-01-08 19:47:11 -0500

    Edited bootstrap style

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ControlPanel from "./components/ControlPanel";
 import Maze from "./models/maze";
 import MAZE_CREATORS from "./constants/maze-creators";
 import ControlPanelData from "./models/control-panel-data";
-import { Col, Container, Form, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 function App() {
   const [maze, setMaze] = useState<Maze | null>();
@@ -24,20 +24,20 @@ function App() {
 
   return (
     <>
-      <h1 className="text-center my-4">Maze Generator</h1>
+      <h1 className="text-center my-4">Minecraft Maze Generator</h1>
       <Container style={{ minHeight: "100vh" }}>
         <Row className="mb-5">
-          <Col md={{ span: 4, offset: 4 }}>
+          <Col md={{ span: 6, offset: 3 }}>
             <ControlPanel onSubmit={handleControlPanelSubmit} />
           </Col>
         </Row>
-        <Row className="flex-grow-1 align-items-center">
-          <Col className="text-center " md={{ span: 6, offset: 3 }}>
+        <Row className="mb-5">
+          <Col className="text-center">
             {maze ? (
               <>
                 <Row>
                   <Col>
-                    Show Solution?{" "}
+                    <span>Show Solution?</span>
                     <input
                       className="mx-2"
                       type="checkbox"
