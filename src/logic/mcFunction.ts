e845e6417b249ae38d77dcdaafe479e753a576d3
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2025-01-12 22:35:01 -0500

    Fixed export

--- src/logic/mcFunction.ts
+++ src/logic/mcFunction.ts
@@ -1,12 +1,24 @@
 import Block from "../models/block";
 
 export function exportToMcFunction(maze: Block[][]) {
-  const content = JSON.stringify(maze, null, 2);
+  const content = maze.reduce(
+    (rowAccum, row, rowIndex) =>
+      rowAccum +
+      row.reduce(
+        (colAccum, blockExists, colIndex) =>
+          blockExists
+            ? colAccum +
+              `\nfill ~${colIndex} ~0 ~${rowIndex} ~${colIndex} ~1 ~${rowIndex} stone`
+            : colAccum,
+        "",
+      ),
+    "",
+  );
   const blob = new Blob([content], { type: "text/plain" });
   const url = window.URL.createObjectURL(blob);
   const link = document.createElement("a");
   link.href = url;
-  link.download = "maze.txt";
+  link.download = "maze.mcfunction";
   link.click();
   window.URL.revokeObjectURL(url);
 }
